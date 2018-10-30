3a944a10252ae18f2bb9d06d0cbe8c558ed7d13f
Ann-Marie Greif <annx9854@stud.kea.dk>
2018-10-30 19:24:14 +0100

    Startet med at kode i javascript

---
+++ script.js
@@ -0,0 +1,54 @@
+function sidenVises() {
+    console.log("sidenVises");
+    //nulstil alting
+
+    //-> showstart
+    showStart();
+
+}
+
+function showStart() {
+    console.log("showStart");
+    //vis startskærm
+
+    document.querySelector("#start").classList.add("show");
+    //start animation på start-knap
+    document.querySelector("#play").classList.add("pulse");
+    //vis settings
+    document.querySelector("#start_settings").classList.add("show");
+
+    document.querySelector("#play").addEventListener("click", hideStart);
+
+    document.querySelector("#start_settings").addEventListener("click", showSettings);
+}
+
+function showSettings() {
+    document.querySelector("#settings").classList.add("show");
+    document.querySelector("#play").addEventListener("click", hideStart);
+    document.querySelector("#settings").addEventListener("click", hideSettings);
+}
+
+function hideSettings() {
+    document.querySelector("#settings").classList.remove("show")
+}
+
+function hideStart() {
+    console.log("hideStart");
+    //Stop animation på start-knap
+    document.querySelector("#play").classList.remove("pulse");
+    //fade startskærm ud
+    document.querySelector("#start").classList.add("fade_out");
+    //hide settings
+    document.querySelector("#settings").classList.remove("show");
+
+    //Når fade animationen er færdig -> startGame
+    document.querySelector("#game_background").addEventListener("animationend", startGame);
+}
+
+function startGame() {
+    console.log("StartGame");
+    //skjul startskærm
+    document.querySelector("#start").classList.add("hide");
+    //vid spilskærm
+    document.querySelector("#game_background").classList.add("show");
+}
