09ee344e4a7f8d9b51a42112590a62c91c0bf16e
Ann-Marie Greif <annx9854@stud.kea.dk>
2018-11-09 11:40:26 +0100

    Færdigt spil

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", sidenVises);
 
 
-
-
 let showSettingsEffektSound = true;
 let showSettingsMusic = true;
 
 function sidenVises() {
     console.log("sidenVises");
+
     //nulstil alting
+
 
     //document.querySelector("#play_again").classList.add("show")
 
@@ -18,6 +18,8 @@ function sidenVises() {
 
     //-> showstart
     showStart();
+
+
 
 }
 
@@ -37,11 +39,6 @@ function showStart() {
     document.querySelector("#start_settings").addEventListener("click", showSettings);
     //klik på settings og vis
 
-
-
-
-document.querySelector("#musik").play();
-
 }
 
 function showSettings() {
@@ -59,7 +56,7 @@ function hideSettings() {
     document.querySelector("#settings").classList.add("hide");
 }
 
-function toggleSounds(){
+function toggleSounds() {
     console.log("toggleSounds");
 
     if (showSettingsEffektSound == false) {
@@ -68,7 +65,7 @@ function toggleSounds(){
         document.querySelector("#sfx_sprite").classList.add("off_on");
 
         document.querySelector("#sfx_sprite").classList.remove("off");
-         document.querySelector("#sfx_sprite").addEventListener("animationend", soundsOn);
+        document.querySelector("#sfx_sprite").addEventListener("animationend", soundsOn);
     } else {
         showSettingsEffektSound = false;
 
@@ -99,23 +96,23 @@ function soundsOn() {
     document.querySelector("#autch").muted = false;
 }
 
-function toggleMusic(){
+function toggleMusic() {
     console.log("showSettingsMusic function " + showSettingsMusic);
 
     if (showSettingsMusic == false) {
         showSettingsMusic = true;
-         document.querySelector("#music_sprite").classList.add("off_on");
+        document.querySelector("#music_sprite").classList.add("off_on");
         document.querySelector("#music_sprite").classList.remove("off");
         document.querySelector("#music_sprite").addEventListener("animationend", musicOn);
-    }else {
+    } else {
         showSettingsMusic = false;
-         document.querySelector("#music_sprite").classList.add("on_off");
+        document.querySelector("#music_sprite").classList.add("on_off");
         document.querySelector("#music_sprite").classList.remove("on");
         document.querySelector("#music_sprite").addEventListener("animationend", musicOff);
     }
 }
 
-function musicOff(){
+function musicOff() {
     console.log("musicOff function værdi er " + showSettingsMusic);
     document.querySelector("#music_sprite").removeEventListener("animationend", musicOff);
     document.querySelector("#music_sprite").classList.remove("on_off");
@@ -124,7 +121,7 @@ function musicOff(){
     document.querySelector("#musik").pause();
 }
 
-function musicOn(){
+function musicOn() {
     console.log("musicOn function værdi er " + showSettingsMusic);
 
     document.querySelector("#music_sprite").removeEventListener("animationend", musicOn);
@@ -139,11 +136,7 @@ function hideStart() {
     //Stop animation på start-knap
     document.querySelector("#play").classList.remove("pulse");
     document.querySelector("#start").classList.add("fade_out");
-
-    //hide settings
-
-
-    //fade startskærm ud
+    document.querySelector("#musik").play();
 
     //Når fade animationen er færdig ->startGame
     document.querySelector("#start").classList.remove("show");
@@ -157,14 +150,33 @@ function startGame() {
     //skjul startskærm
 
     //vid spilskærm
-    document.querySelector("#start").classList = "hide"; document.querySelector("#game_background").classList.add("show");
+    document.querySelector("#start").classList = "hide";
+    document.querySelector("#game_background").classList.add("show");
 
     document.querySelector("#butterflyblue").addEventListener("click", clickButterfly);
     document.querySelector("#butterflyorange").addEventListener("click", clickButterfly);
     document.querySelector("#angrybee").addEventListener("click", clickBee);
     document.querySelector("#fatbee").addEventListener("click", clickBee);
 
+    tidenGaar();
 
+
+}
+
+let timeLeft = 20;
+
+function tidenGaar() {
+    console.log("tidenGaar");
+    timeLeft--;
+
+    if (timeLeft > 0) {
+        setTimeout(tidenGaar, 1000);
+    } else {
+        gameOver();
+    }
+
+    console.log(timeLeft);
+    document.querySelector("#tid").textContent = timeLeft;
 }
 
 let point = 0;
@@ -172,7 +184,9 @@ let point = 0;
 function clickButterfly() {
 
     console.log("clickButterfly");
-
+    this.classList.remove("butterflyblue_fly");
+    this.classList.remove("butterflyorange_fly");
+    this.classList.add("hide_butterfly");
 
     point++;
     console.log(point);
@@ -183,31 +197,37 @@ function clickButterfly() {
     if (this.classList.contains("butterflyblue_fly")) {
         document.querySelector("#yay").currentTime = 0;
         document.querySelector("#yay").play();
-        }
-        if (this.classList.contains("butterflyorange_fly")) {
+    }
+    if (this.classList.contains("butterflyorange_fly")) {
         document.querySelector("#yay").currentTime = 0;
         document.querySelector("#yay").play();
 
-           }
- //Hvordan får jeg figurerne til at starte igen?
+    }
+    //Hvordan får jeg figurerne til at starte igen?
+    this.addEventListener("animationend", resetButterfly)
 
-          //console.log(this);
-        //this.classList.add("hide");
-    //this.addEventListener("animationend", butterflyForfra);
 
 }
 
-function butterflyForfra(){
-   // console.log("butterflyForfra");
-  //  console.log(this);
-  //  this.classList.add("show");
+function resetButterfly() {
+    console.log("reset")
+    this.className = "";
+    document.querySelector("#butterflyblue").classList.add("butterflyblue_fly");
+    document.querySelector("#butterflyorange").classList.add("butterflyorange_fly");
 }
+
+
+
 
 let liv = 3;
 
 
 function clickBee() {
     console.log("clickBee");
+
+    this.classList.remove("fatbee_fly");
+    this.classList.remove("angrybee_fly");
+    this.classList.add("hide_butterfly");
 
     let heart = "#heart" + liv;
 
@@ -224,14 +244,22 @@ function clickBee() {
     if (this.classList.contains("angrybee_fly")) {
         document.querySelector("#autch").currentTime = 0;
         document.querySelector("#autch").play();
-        }
-        if (this.classList.contains("fatbee_fly")) {
+    }
+    if (this.classList.contains("fatbee_fly")) {
         document.querySelector("#autch").currentTime = 0;
         document.querySelector("#autch").play();
+    }
+    this.addEventListener("animationend", resetBee)
+}
 
- }
+function resetBee() {
+    console.log("resetBee");
 
- }
+    this.className = "";
+    document.querySelector("#fatbee").classList.add("fatbee_fly");
+    document.querySelector("#angrybee").classList.add("angrybee_fly");
+}
+
 
 function gameStatus() {
     if (point < 1) {
@@ -250,8 +278,17 @@ function gameOver() {
     console.log("gameOver");
 
     document.querySelector("#gameover").classList.remove("hide");
+    document.querySelector("#game_background").classList.add("blur");
+    document.querySelector("#butterflyblue").classList.add("hide");
+    document.querySelector("#butterflyorange").classList.add("hide");
+    document.querySelector("#angrybee").classList.add("hide");
+    document.querySelector("#fatbee").classList.add("hide");
+    document.querySelector("#tid").classList.add("hide");
 
     document.querySelector("#retry").addEventListener("click", sidenVises);
+    document.querySelector("#retry").addEventListener("click", reset);
+
+
 
 
 }
@@ -260,6 +297,18 @@ function levelComplete() {
     console.log("levelComplete");
 
     document.querySelector("#levelcomplete").classList.remove("hide");
+    document.querySelector("#game_background").classList.add("blur");
+    document.querySelector("#butterflyblue").classList.add("hide");
+    document.querySelector("#butterflyorange").classList.add("hide");
+    document.querySelector("#angrybee").classList.add("hide");
+    document.querySelector("#fatbee").classList.add("hide");
+    document.querySelector("#tid").classList.add("hide");
 
     document.querySelector("#play_again").addEventListener("click", sidenVises);
+
+    document.querySelector("#play_again").addEventListener("click", reset);
+}
+
+function reset() {
+    window.history.go(0);
 }
